928d4d660031fd1a1843f8e54f666d5fec5d7bf0
Yegor <petrov.yegor@gmail.com>
2016-01-27 16:16:16 +0500

    Change target to 4.0 for increased compatibility

--- ModbusImport/Properties/AssemblyInfo.cs
+++ ModbusImport/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2016.1.27.1031")]
-[assembly: AssemblyFileVersion("2016.1.27.1031")]
+[assembly: AssemblyVersion("2016.1.27.1059")]
+[assembly: AssemblyFileVersion("2016.1.27.1059")]
